34e42ace46b5826583daf79ae8fff4591a38d6b5
Rochel Munitz <rochelmunitz@gmail.com>
2025-01-22 20:04:50 -0800

    first attempt

--- search/graph.py
+++ search/graph.py
@@ -1,4 +1,6 @@
+from matplotlib import pyplot as plt
 import networkx as nx
+from collections import deque
 
 class Graph:
     """
@@ -11,18 +13,169 @@ class Graph:
         Initialization of graph object 
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        
+
+    #returns the neighbors of a given node
+    
+    def get_neighbors(self, node):
+        '''
+        returns a list of all the neighbors of a given node
+        '''
+        return list(self.graph.neighbors(node))
+    
+        
 
     def bfs(self, start, end=None):
         """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+        This method performs a breadth first traversal and pathfinding on graph G
 
-        * If there's no end node input, return a list nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, return None
+        * If there's no end node input, returns a list of nodes with the order of BFS traversal
+        * If there is an end node input and a path exists, returns a list of nodes with the order of the shortest path
+        * If there is an end node input and a path does not exist, returns None
 
         """
-        return
+
+       
+        '''
+            #pseudocode for breadth first trasversal (from class)
+            def BFS(G, source):
+            Q = queue()
+            visited = [ ]
+            Q.push(source)
+            visited.append(source)
+            while Q is not empty:
+                v = Q.pop()
+                N = neighbors(v)
+                for all w in N:
+                    if w not in visited:
+                        visited.append(w)
+                        Q.push(w)
+        #missing method to keep track of parent nodes to track shortest path
+        '''
+        
+        #edge cases
+        #empty graph
+        if len(self.graph) == 0:
+            raise ValueError("Empty graph")
+            
+
+        #unconnected graph
+        #TO DO
 
 
+        #start node not in graph
+        if start not in self.graph: #check this works as expected
+            raise ValueError("Start node not in graph")
+            
+        
+        #end node not in graph
+        if end not in self.graph and end != None:
+            raise ValueError("End node not in graph")
+            return None
+        
+        #start node = end node
+        if start == end:
+            raise ValueError("Start node = End node!")
+
+
+        #breadth first traversal
+       
+        #initialize a queue
+        #use deque for more efficiency? or stick with list? 
+        #queue = queue()
+        queue = deque() 
+
+        #initialize a list to store visited nodes
+        visited_nodes =  []
+       
+        #dictionary to store parent nodes (used to reconstruct shortest path) 
+        parent = {}
+
+        #add the start node to the queue
+        queue.append(start)
+        #queue.push(start)
+
+        #add the start node to the visited list
+        visited_nodes.append(start)
+
+        #parent node of start = None
+        parent[start] = None
+
+        #while the queue is not empty
+        while queue:
+            #pop the first node in the queue
+            #current_node = queue.pop(0)
+            current_node = queue.popleft() #efficiency 0(1)
+
+            # stop if reached end
+            if current_node == end:
+                break
+
+            #get all neighboring nodes
+            neighbors = self.get_neighbors(current_node) 
+            
+            # for each neighbor, check if it was visited, if not, add to visited list and queue
+            for neighbor in neighbors:
+                if neighbor not in visited_nodes: 
+                    visited_nodes.append(neighbor)
+                    #queue.push(neighbor)
+                    queue.append(neighbor)
+                    parent[neighbor] = current_node #keeping track of path
+                    
+        
+        #if no end node is given, return list of visited nodes in order of traversal
+        if end == None:
+            return list(visited_nodes)
+        
+        #if end node is reachable, return the shorest path
+        if end in visited_nodes:
+            path = []
+            #backtrack from end node to start node
+            while end != None:
+                path.append(end)
+                end = parent[end]
+            #reverse path to get from start to end
+            path.reverse()
+            return path
+        else:
+            print("No path found")
+            return None
+                
+
+
+def main():
+    """
+    Main function to run the code
+    """
+    #extra code to test Graph class
+    tiny_network_graph = Graph('data/tiny_network.adjlist')
+    #print(tiny_network_graph.graph)
+    #print(tiny_network_graph.get_neighbors('Luke Gilbert'))
+    #print(tiny_network_graph.bfs('Luke Gilbert','Martin Kampmann'))
+
+    citation_network_graph = Graph('data/citation_network.adjlist')
+    print((citation_network_graph.bfs('32024998','Tony Capra')))
+
+    '''
+    Extra, plotting to help visualize graph 
+    #plot graph
+    plt.figure(figsize=(12, 8))
+    nx.draw(
+        tiny_network_graph.graph, 
+        with_labels=True, 
+        node_size=500, 
+        node_color="lightblue", 
+        font_size=8, 
+        font_weight="bold", 
+        edge_color="gray"
+    )
+    plt.title("Graph Visualization")
+    plt.show()
+    '''
+
+    pass
+
+if __name__ == "__main__":
+    main()
 
 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -10,6 +10,23 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+    #testing trasversal, only provide start node
+
+    #create Graph object
+    tiny_network_graph = graph.Graph('data/tiny_network.adjlist')
+    #test bfs traversal valid start node
+    start_node = 'Luke Gilbert'
+    assert tiny_network_graph.bfs(start=start_node) == ['Luke Gilbert', '33483487', '31806696', '31626775', '31540829', 'Martin Kampmann', 'Neil Risch', 
+                                                        'Nevan Krogan', '32790644', '29700475', '34272374', '32353859', '30944313', 'Steven Altschuler',
+                                                          'Lani Wu', 'Michael Keiser', 'Atul Butte', 'Marina Sirota', 'Hani Goodarzi', '32036252', '32042149', 
+                                                          '30727954', '33232663', '33765435', '33242416', '31395880', '31486345', 'Michael McManus', 'Charles Chiu', '32025019']
+    
+    #test bfs traversal invalid start node
+    start_node = 'Rochel Munitz'
+    with pytest.raises(ValueError, match = "Start node not in graph"):
+        tiny_network_graph.bfs(start=start_node)
+
+
     pass
 
 def test_bfs():
@@ -23,4 +40,13 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
+    #testing bfs provides shortest path, valid start and end nodes
+    citation_network_graph = graph.Graph('data/citation_network.adjlist')
+    start_node = '32024998'
+    end_node = 'Tony Capra'
+    assert citation_network_graph.bfs(start=start_node, end=end_node) == ['32024998', 'Atul Butte', '32025013', 'Tony Capra']
+
+    #add tests 
+    
     pass
+
