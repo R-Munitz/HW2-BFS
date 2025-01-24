56b198c300f7509ba64df7955cfefa701a5a54d9
Rochel Munitz <rochelmunitz@gmail.com>
2025-01-23 21:17:30 -0800

    refactored lots of code to add edge case, abstracted methods

--- search/graph.py
+++ search/graph.py
@@ -8,34 +8,53 @@ class Graph:
     
     You may add any functions you deem necessary to the class
     """
+
     def __init__(self, filename: str):
         """
         Initialization of graph object 
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
-        
-
-    #returns the neighbors of a given node
     
+    #returns the neighbors of a given node
     def get_neighbors(self, node):
         '''
         returns a list of all the neighbors of a given node
         '''
         return list(self.graph.neighbors(node))
     
+    #checks if graph is unconnected
+    def is_unnconnected(self, start):
+        '''
+        returns True if the graph is unconnected, False otherwise
+        '''
+        visited_nodes = self.run_bfs(start) 
+        if len(visited_nodes) != len(self.graph):
+            return True
+        else:
+            return False
+
+    #handles edge cases   
+    def validate_input(self, start, end=None):
+
+        #empty graph
+        if len(self.graph) == 0:
+            raise ValueError("Empty graph")
         
+        #start node not in graph
+        elif start not in self.graph: #check this works as expected
+            raise ValueError("Start node not in graph")
+            
+        #end node not in graph
+        elif end not in self.graph and end != None:
+            raise ValueError("End node not in graph")
+    
+        #start node = end node
+        elif start == end:
+            raise ValueError("Start node = End node!")
+    
+    #breath first traversal
+    def run_bfs(self, start, end=None):
 
-    def bfs(self, start, end=None):
-        """
-        This method performs a breadth first traversal and pathfinding on graph G
-
-        * If there's no end node input, returns a list of nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, returns a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, returns None
-
-        """
-
-       
         '''
             #pseudocode for breadth first trasversal (from class)
             def BFS(G, source):
@@ -52,16 +71,102 @@ class Graph:
                         Q.push(w)
         #missing method to keep track of parent nodes to track shortest path
         '''
+
+        #initialize a queue
+        #use deque for more efficiency? or stick with list? 
+        #queue = []
+        queue = deque() 
+
+        #initialize a list to store visited nodes
+        visited_nodes =  []
+       
+        #dictionary to store parent nodes (used to reconstruct shortest path) 
+        parent = {}
+
+        #add the start node to the queue
+        queue.append(start)
+        #queue.push(start)
+
+        #add the start node to the visited list
+        visited_nodes.append(start)
+
+        #set parent node of start = None
+        parent[start] = None
+
+        #while the queue is not empty
+        while queue:
+            #pop the first node in the queue
+            #current_node = queue.pop(0)
+            current_node = queue.popleft() #efficiency 0(1)
+
+            # stop if reached end
+            if current_node == end:
+                break
+
+            #get all neighboring nodes
+            neighbors = self.get_neighbors(current_node) 
+            
+            # for each neighbor, check if it was visited, if not, add to visited list and queue
+            for neighbor in neighbors:
+                if neighbor not in visited_nodes: 
+                    visited_nodes.append(neighbor)
+                    #queue.push(neighbor)
+                    queue.append(neighbor)
+                    parent[neighbor] = current_node  #keeping track of path
+
+                    
+        #if no end node is given, return list of visited nodes in order of traversal
+        if end == None:
+            return list(visited_nodes)
         
+        #if end node is reachable, return the shorest path
+        if end in visited_nodes:
+            path = []
+            #backtrack from end node to start node
+            while end != None:
+                path.append(end)
+                end = parent[end]
+            #reverse path to get from start to end
+            path[::1]
+            return path
+        else:
+            print("No path found")
+            return None
+                
+
+    def bfs(self, start, end=None):
+        """
+        This method performs a breadth first traversal and pathfinding on graph G
+
+        * If there's no end node input, returns a list of nodes with the order of BFS traversal
+        * If there is an end node input and a path exists, returns a list of nodes with the order of the shortest path
+        * If there is an end node input and a path does not exist, returns None
+
+        """
+        
+        #abstracted  edge cases to new method
+        #ensure valid input
+        self.validate_input(start, end)  
+
+       
+        #if graph is unconnected and end=None, raise error. cannot fully traverse unconnected graph
+        if end == None:
+            #ensure graph is connected
+            if self.is_unnconnected(start):
+                raise ValueError("Graph is unconnected, cannot traverse")
+        
+        #run bfs traversal
+        path = self.run_bfs(start,end)
+        return path  #path will be None if no path is found
+        
+
+        '''
+        #abstracted to new method
         #edge cases
         #empty graph
         if len(self.graph) == 0:
             raise ValueError("Empty graph")
-            
-
-        #unconnected graph
-        #TO DO
-
+        
 
         #start node not in graph
         if start not in self.graph: #check this works as expected
@@ -71,13 +176,17 @@ class Graph:
         #end node not in graph
         if end not in self.graph and end != None:
             raise ValueError("End node not in graph")
-            return None
+    
         
         #start node = end node
         if start == end:
             raise ValueError("Start node = End node!")
+        
 
+        '''
 
+        '''
+        #abstacted to new method
         #breadth first traversal
        
         #initialize a queue
@@ -121,6 +230,7 @@ class Graph:
                     #queue.push(neighbor)
                     queue.append(neighbor)
                     parent[neighbor] = current_node #keeping track of path
+
                     
         #if no end node is given, return list of visited nodes in order of traversal
         if end == None:
@@ -139,28 +249,30 @@ class Graph:
         else:
             print("No path found")
             return None
-                
+    '''            
 
 
 def main():
     """
-    Main function to run the code
+    extra code to test Graph class
     """
-    #extra code to test Graph class
+   
     tiny_network_graph = Graph('data/tiny_network.adjlist')
-    #print(tiny_network_graph.graph)
+    print(tiny_network_graph.graph)
     #print(tiny_network_graph.get_neighbors('Luke Gilbert'))
     #print(tiny_network_graph.bfs('Luke Gilbert','Martin Kampmann'))
 
     citation_network_graph = Graph('data/citation_network.adjlist')
-    print((citation_network_graph.bfs('32024998','Tony Capra')))
+    print((citation_network_graph.bfs('32024998','34095129')))
+    #print(citation_network_graph.is_unnconnected('Tony Capra'))
+    #print((citation_network_graph.bfs('Tony Capra')))
 
     '''
-    Extra, plotting to help visualize graph 
+   #Extra code, plotting to help visualize graph 
     #plot graph
     plt.figure(figsize=(12, 8))
     nx.draw(
-        tiny_network_graph.graph, 
+        citation_network_graph.graph, 
         with_labels=True, 
         node_size=500, 
         node_color="lightblue", 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,6 +1,7 @@
 # write tests for bfs
 import pytest
 from search import graph
+import networkx as nx
 
 def test_bfs_traversal():
     """
@@ -10,12 +11,16 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    #testing trasversal, only provide start node
+    #testing trasversal of all nodes in graph
 
     #create Graph object
     tiny_network_graph = graph.Graph('data/tiny_network.adjlist')
+
     #test bfs traversal valid start node
     start_node = 'Luke Gilbert'
+    #assert correct number of nodes are traversed
+    assert len(tiny_network_graph.bfs(start=start_node)) == 30
+    #assert correct order of traversal
     assert tiny_network_graph.bfs(start=start_node) == ['Luke Gilbert', '33483487', '31806696', '31626775', '31540829', 'Martin Kampmann', 'Neil Risch', 
                                                         'Nevan Krogan', '32790644', '29700475', '34272374', '32353859', '30944313', 'Steven Altschuler',
                                                           'Lani Wu', 'Michael Keiser', 'Atul Butte', 'Marina Sirota', 'Hani Goodarzi', '32036252', '32042149', 
@@ -26,6 +31,11 @@ def test_bfs_traversal():
     with pytest.raises(ValueError, match = "Start node not in graph"):
         tiny_network_graph.bfs(start=start_node)
 
+    #test bfs traversal unconnected graph
+    citation_network_graph = graph.Graph('data/citation_network.adjlist') #this is an unconnected graph
+    start_node = 'Tony Capra'
+    with pytest.raises(ValueError, match = "Graph is unconnected, cannot traverse"):
+        citation_network_graph.bfs(start=start_node)
 
     pass
 
@@ -40,13 +50,43 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    #testing bfs provides shortest path, valid start and end nodes
+
+    #testing bfs provides shortest path
+    
+
+    # valid start and end nodes
     citation_network_graph = graph.Graph('data/citation_network.adjlist')
     start_node = '32024998'
     end_node = 'Tony Capra'
-    assert citation_network_graph.bfs(start=start_node, end=end_node) == ['32024998', 'Atul Butte', '32025013', 'Tony Capra']
+    assert citation_network_graph.bfs(start=start_node, end=end_node) == ['32024998', 'Atul Butte', '32025013', 'Tony Capra'] 
 
-    #add tests 
+    #test invalid end node
+    end_node = 'Rochel Munitz'
+    with pytest.raises(ValueError, match = "End node not in graph"):
+        citation_network_graph.bfs(start=start_node, end=end_node)
+
+    #test start node = end node
+    with pytest.raises(ValueError, match = "Start node = End node!"):
+        citation_network_graph.bfs(start=start_node, end=start_node)
     
+
+    #test unreachable end node / unconnected graph returns None
+
+    #create graph with unreachable node
+    unconnected_graph = nx.DiGraph()
+
+    #add nodes and edges
+    unconnected_graph.add_nodes_from([1, 2, 3, 4, 5])
+    unconnected_graph.add_edges_from([(1, 2), (2, 3), (3, 4)])
+
+    #add unreachable node
+    unconnected_graph.add_node(5)
+
+    #test bfs search returns None for unreachable node
+    start_node = '1'
+    end_node = '5'
+    assert unconnected_graph.bfs(start=start_node, end=end_node) == None
+
+
     pass
 
